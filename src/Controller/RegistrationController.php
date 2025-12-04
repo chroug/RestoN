7377979c2338844eb94c79ea2f751626046019dc
gerbaux valentin <valentin.gerbaux@etudiant.univ-reims.fr>
2025-12-04 22:37:23 +0100

    modification du formulaire de Register pour inclure les attributs obligatoire

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -77,8 +77,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Email Vérifiée !!!.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_plats'); //retour aux plats
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,30 +18,36 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class)
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('telephone', TextType::class, [
+                'required' => false,
+                'label' => 'Téléphone (optionnel)'
+            ])
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => 'J\'accepte les conditions d\'utilisation',
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+
+                    new IsTrue(message: 'Vous devez accepter nos conditions d\'utilisation pour continuer.'),
                 ],
             ])
+
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                    // CORRECTION NotBlank
+                    new NotBlank(message: 'Veuillez entrer un mot de passe'),
+
+                    // CORRECTION Length
+                    new Length(
+                        min: 6,
+                        minMessage: 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        max: 4096
+                    ),
                 ],
             ])
         ;
@@ -49,6 +56,7 @@ class RegistrationFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+
             'data_class' => User::class,
         ]);
     }
