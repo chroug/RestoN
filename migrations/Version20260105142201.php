c46dc5644d181824b82fa04a3ecfabb027b13523
Timothé Pierret <timothe.pierret@etudiant.univ-reims.fr>
2026-01-05 15:48:02 +0100

    Fin de la modification des commandes avec le status, le total et le details des commandes

--- migrations/Version20260105142201.php
+++ migrations/Version20260105142201.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260105134130 extends AbstractMigration
+final class Version20260105142201 extends AbstractMigration
 {
     public function getDescription(): string
     {
