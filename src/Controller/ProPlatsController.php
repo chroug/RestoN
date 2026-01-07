ec3eebc49df3037f16087b0b7531c88af4697e58
gerbaux valentin <valentin.gerbaux@etudiant.univ-reims.fr>
2026-01-07 11:00:14 +0100

    modification du controller pour editer et supprimer les plats

--- src/Controller/ProPlatsController.php
+++ src/Controller/ProPlatsController.php
@@ -8,10 +8,12 @@ use App\Repository\PlatsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/patron/plats')]
 #[IsGranted('ROLE_PATRON')]
@@ -54,5 +56,66 @@ class ProPlatsController extends AbstractController
             'plat' => $plat,
             'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_pro_plats_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Plats $plat, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+
+        if ($plat->getRestaurant() !== $this->getUser()->getRestaurant()) {
+            throw $this->createAccessDeniedException("Vous ne pouvez pas modifier le plat d'un autre restaurant.");
+        }
+
+        $form = $this->createForm(PlatsType::class, $plat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $imageFile = $form->get('image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('kernel.project_dir').'/public/uploads/plats',
+                        $newFilename
+                    );
+
+                    $plat->setImage($newFilename);
+                } catch (FileException $e) {
+
+                }
+            }
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le plat a été modifié avec succès !');
+            return $this->redirectToRoute('app_pro_plats_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('pro_plats/edit.html.twig', [
+            'plat' => $plat,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_pro_plats_delete', methods: ['POST'])]
+    public function delete(Request $request, Plats $plat, EntityManagerInterface $entityManager): Response
+    {
+
+        if ($plat->getRestaurant() !== $this->getUser()->getRestaurant()) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$plat->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($plat);
+            $entityManager->flush();
+            $this->addFlash('success', 'Le plat a été supprimé.');
+        }
+
+        return $this->redirectToRoute('app_pro_plats_index', [], Response::HTTP_SEE_OTHER);
     }
 }
